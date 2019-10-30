0eca0bf65090d3ea5f3ff0cf6b2f2158ebc6b8b8
junger <1357114293@qq.com>
2019-10-30 13:49:54 +0800

    项目创建,第一次提交 2019年10月30日11:57:41

---
+++ app/src/main/java/com/l900/master/page/TabFragmentPagerAdapter.java
@@ -0,0 +1,30 @@
+package com.l900.master.page;
+
+
+
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+public class TabFragmentPagerAdapter extends FragmentPagerAdapter {
+    private FragmentManager mfragmentManager;
+    private List<Fragment> mlist;
+
+    public TabFragmentPagerAdapter(FragmentManager fm, List<Fragment> list) {
+        super(fm);
+        this.mlist = list;
+    }
+
+    @Override
+    public Fragment getItem(int arg0) {
+        return mlist.get(arg0);//显示第几个页面
+    }
+
+    @Override
+    public int getCount() {
+        return mlist.size();//有几个页面
+    }
+}
